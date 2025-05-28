cc7906d14dfa56d545e133de8d8c5ada2fa813b8
Mikael Souza Silva <mikaelzzzz@MacBook-Pro-de-Mikael.local>
2025-05-28 20:25:38 -0300

    fix: adiciona prefixo /api em todas as rotas

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from app.api.endpoints import whatsapp
 
 app = FastAPI()
 
-app.include_router(webhook.router, prefix="/webhook", tags=["Webhook"])
-app.include_router(financeiro.router, prefix="/financeiro", tags=["Financeiro"])
-app.include_router(flexge.router, prefix="/flexge", tags=["Flexge"])
-app.include_router(gramatica.router, prefix="/gramatica", tags=["Gramatica"])
-app.include_router(imagem.router, prefix="/imagem", tags=["Imagem"])
-app.include_router(voice.router, prefix="/voice", tags=["Voice"])
-app.include_router(whatsapp.router, prefix="/whatsapp", tags=["WhatsApp"]) 
+app.include_router(webhook.router, prefix="/api/webhook", tags=["Webhook"])
+app.include_router(financeiro.router, prefix="/api/financeiro", tags=["Financeiro"])
+app.include_router(flexge.router, prefix="/api/flexge", tags=["Flexge"])
+app.include_router(gramatica.router, prefix="/api/gramatica", tags=["Gramatica"])
+app.include_router(imagem.router, prefix="/api/imagem", tags=["Imagem"])
+app.include_router(voice.router, prefix="/api/voice", tags=["Voice"])
+app.include_router(whatsapp.router, prefix="/api/whatsapp", tags=["WhatsApp"]) 
